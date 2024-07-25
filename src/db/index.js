e2a3916b817dd9d64a5c9068d0f5f2bee5e3faa5
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-07-25 19:08:37 +0500

    Express App started

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ async function dbConnection() {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
     console.log("Database Connection Successfull, DB Host::", connectionInstance.connection.host);
-    process.exit(0);
   } catch (error) {
     console.log("Database Connection Failed :: line 10, db/index.js::", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dbConnection from "./db/index.js";
 import dotenv from "dotenv";
-dotenv.config({path:'./env'});
+dotenv.config({ path: "./.env" });
+import app from "./app.js";
 
 // 2nd approach of Database Connection
-dbConnection();
+dbConnection() //async function returns a promise we can run .then and .catch on it.
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server successfully started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database Connection Failed :: line 10 index.js::", error);
+  });
 
 // 1st approach of Database Connection
 // (async () => {
