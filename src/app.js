ec68c3edc56fb3d61639466f2861fef70801563c
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-07-27 14:12:53 +0500

    Configured Express App

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import express from "express";
 const app = express();
+
+app.use(
+  cors({
+    origin: `${process.env.CORS_ORIGIN}`,
+    credentials: true, //server allows credentials to be included in cross-origin requests. Credentials can include cookies, authorization headers, or TLS client certificates.
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export default app;
