325af874c8f89f0f2dd06e7590ba0a27f880b52a
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-23 16:00:55 +0500

    Added functionalities to create,update & delete tweet.

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,11 +5,58 @@ import { Tweet } from "../models/tweet.model.js";
 import mongoose, { isValidObjectId } from "mongoose";
 
 const createTweet = asyncHandler(async (req, res) => {
-    
+  const user = req.user;
+  const { content } = req.body;
 
-    res.send("work in progress of creat tweet")
-})
+  if (!content?.trim()) {
+    throw new ApiError("Tweet is empty, must enter anything!", 400);
+  }
 
-export {
-    createTweet
-}
+  const tweet = await Tweet.create({
+    owner: user._id,
+    content,
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Created tweet Successfully!"));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { newContent } = req.body;
+
+  if (!newContent?.trim()) {
+    throw new ApiError("Must enter anything to update Twwet!", 400);
+  }
+
+   // Validate tweetId
+   if (!isValidObjectId(tweetId)) {
+    throw new ApiError("Invalid Tweet ID", 400);
+  }
+
+  const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      content: newContent,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res
+  .status(200)
+  .json(new ApiResponse(200, updatedTweet, "Updated tweet Successfully!"));
+
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+   await Tweet.findByIdAndDelete(tweetId);
+  res
+  .status(200)
+  .json(new ApiResponse(200, {}, "Tweet Deleted Successfully!"));
+});
+
+export { createTweet, updateTweet, deleteTweet };
--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, updateTweet, deleteTweet  } from "../controllers/tweet.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/create-tweet").post(createTweet)
+router.route("/update-tweet/:tweetId").patch(updateTweet)
+router.route("/delete-tweet/:tweetId").delete(deleteTweet)
 
 
 
