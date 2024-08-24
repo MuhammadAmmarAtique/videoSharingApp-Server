65816a8a43cb8cb233cc85d0e965ae43e7918e8e
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-24 14:19:32 +0500

    Added functionality to add & delete playlist.

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -92,37 +92,89 @@ const getPlaylistById = asyncHandler(async (req, res) => {
   }
 
   res
-  .status(200)
-  .json(
-    new ApiResponse(
-      200,
-      playlist,
-      "Successfully fetched Playlist from database!"
-    )
-  );
-
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        playlist,
+        "Successfully fetched Playlist from database!"
+      )
+    );
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-  const user = req.user
-  const userId = user._id
+  const user = req.user;
+  const userId = user._id;
   if (!isValidObjectId(userId)) {
-    throw new ApiError("Invalid userId!" , 400)
+    throw new ApiError("Invalid userId!", 400);
   }
   const userPlaylists = await Playlist.find({
-    owner: new mongoose.Types.ObjectId(userId)
-  })
+    owner: new mongoose.Types.ObjectId(userId),
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        userPlaylists,
+        "Successfully fetched all User Playlist from database!"
+      )
+    );
+});
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+  
+  if (!name?.trim() &&  !description?.trim()) {
+    throw new ApiError("Must give either one of name or description to update playlist! ", 400);
+  }
+
+  const playlist = await Playlist.findById(playlistId)
+  
+  if (name) {
+    playlist.name = name
+    await playlist.save()
+  }
+
+  if (description) {
+    playlist.description = description
+    await playlist.save()
+  }
 
   res
   .status(200)
   .json(
     new ApiResponse(
       200,
-      userPlaylists,
-      "Successfully fetched all User Playlist from database!"
+      playlist,
+      "Successfully updated Playlist!"
     )
   );
- 
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+  const {playlistId} = req.params
+  await Playlist.findByIdAndDelete(playlistId);
+
+   res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      {},
+      "Successfully deleted Playlist!"
+    )
+  );
 })
 
-export { createPlaylist, addVideoToPlaylist, removeVideoFromPlaylist, getPlaylistById, getUserPlaylists };
+export {
+  createPlaylist,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  updatePlaylist,
+  deletePlaylist,
+};
--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,14 +1,28 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
-import { createPlaylist, addVideoToPlaylist, removeVideoFromPlaylist, getPlaylistById, getUserPlaylists } from "../controllers/playlist.controller.js";
+import {
+  createPlaylist,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  updatePlaylist,
+  deletePlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/create-playlist").post(createPlaylist);
-router.route("/add-video-to-playlist/:playlistId/:videoId").post(addVideoToPlaylist);
-router.route("/remove-video-from-playlist/:playlistId/:videoId").patch(removeVideoFromPlaylist);
+router
+  .route("/add-video-to-playlist/:playlistId/:videoId")
+  .post(addVideoToPlaylist);
+router
+  .route("/remove-video-from-playlist/:playlistId/:videoId")
+  .patch(removeVideoFromPlaylist);
 router.route("/get-playlist-by-Id/:playlistId").get(getPlaylistById);
 router.route("/get-user-playlist").get(getUserPlaylists);
+router.route("/update-playlist/:playlistId").patch(updatePlaylist);
+router.route("/delete-playlist/:playlistId").delete(deletePlaylist);
 
 export default router;
