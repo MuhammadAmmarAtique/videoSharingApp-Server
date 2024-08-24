1a78477df2c90c14fb215f19f208a0c2ca7dc3ad
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-24 13:30:37 +0500

    Added controllers to add and remove video from playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -20,10 +20,60 @@ const createPlaylist = asyncHandler(async (req, res) => {
     owner: user._id,
   });
 
-  console.log("playlist: ", playlist);
   res
     .status(200)
     .json(new ApiResponse(200, playlist, "Playlist created Successfully!"));
 });
 
-export { createPlaylist };
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $push: { videos: videoId }, //push operator adds videoId to existing array
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedPlaylist) {
+    throw new ApiError(
+      "Problem during updating Playlist!",
+      500
+    );
+  }
+
+  res
+  .status(200)
+  .json(new ApiResponse(200, updatedPlaylist, "Added video in Playlist Successfully!"));
+});
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: { videos: videoId }, // pull operator will remove videoId from  array
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedPlaylist) {
+    throw new ApiError(
+      "Problem during updating Playlist!",
+      500
+    );
+  }
+
+  res
+  .status(200)
+  .json(new ApiResponse(200, updatedPlaylist, "Removed video from Playlist Successfully!"));
+});
+
+
+export { createPlaylist, addVideoToPlaylist, removeVideoFromPlaylist };
--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,9 +1,11 @@
-import { createPlaylist } from "../controllers/playlist.controller.js";
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
+import { createPlaylist, addVideoToPlaylist, removeVideoFromPlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 router.route("/create-playlist").post(createPlaylist);
+router.route("/add-video-to-playlist/:playlistId/:videoId").post(addVideoToPlaylist);
+router.route("/remove-video-from-playlist/:playlistId/:videoId").patch(removeVideoFromPlaylist);
 
 export default router;
