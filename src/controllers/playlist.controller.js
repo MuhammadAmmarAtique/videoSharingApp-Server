747c48f0c330c0c1b52e70fa73c31dff4af6d762
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-24 12:45:02 +0500

    Added "createPlaylist" Controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,11 +1,29 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Playlist } from "../models/playlist.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
+  const user = req.user;
 
-  res.send("work in progress!")
+  if (!name?.trim() || !description?.trim()) {
+    throw new ApiError(
+      "Both name and description is must required for creating Playlist!",
+      400
+    );
+  }
+
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: user._id,
+  });
+
+  console.log("playlist: ", playlist);
+  res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist created Successfully!"));
 });
 
 export { createPlaylist };
