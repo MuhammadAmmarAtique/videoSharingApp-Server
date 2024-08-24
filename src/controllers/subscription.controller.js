e51c803e42dc030cc3eb8046c1ead465b704490f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-24 22:52:31 +0500

    Added subscribeChannel and unsubscribeChannel Controllers.

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,13 +4,55 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { Subscription } from "../models/subscription.model.js";
 import mongoose, { isValidObjectId } from "mongoose";
 
+const subscribeChannel = asyncHandler(async (req, res) => {
+  const user = req.user;
+  const { channelId } = req.params;
+
+  const subscription = await Subscription.create({
+    subscriber: user._id,
+    channel: channelId,
+  });
+
+  if (!subscription) {
+    throw new ApiError("Problem in subscribing Chaneel!", 500);
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscription,
+        "User has successfully Subscribed a Channel! "
+      )
+    );
+});
+
+const unsubscribeChannel = asyncHandler(async (req, res) => {
+  const user = req.user;
+  const { channelId } = req.params;
+
+  const subscription = await Subscription.findOne({
+    subscriber: user._id,
+    channel: channelId,
+  });
+
+  if (!subscription) {
+    throw new ApiError("User is already unsubscribed!", 400);
+  }
+  await subscription.deleteOne();
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, {}, "User has successfully unSubscribed a Channel! ")
+    );
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+  const { channelId } = req.params;
+  res.send("work in progresss!!!");
+});
 
-
-    res.send("work in progresss!!!")
-})
-
-export {getUserChannelSubscribers}
+export { subscribeChannel, unsubscribeChannel, getUserChannelSubscribers };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
-import {getUserChannelSubscribers} from '../controllers/subscription.controller.js'
+import {
+  subscribeChannel,
+  unsubscribeChannel,
+  getUserChannelSubscribers,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/get-user-channel-subscribers").get(getUserChannelSubscribers)
-
-
+router.route("/subscribe-channel/:channelId").post(subscribeChannel);
+router.route("/unsubscribe-channel/:channelId").delete(unsubscribeChannel);
+router.route("/get-user-channel-subscribers").get(getUserChannelSubscribers);
 
 export default router;
