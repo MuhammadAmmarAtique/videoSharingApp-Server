d5bd64eed305e583593e4ed84eb2bdd73136b1cc
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-08-25 23:59:18 +0500

    Added "getUserSubscribedChannels" controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,7 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Subscription } from "../models/subscription.model.js";
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose from "mongoose";
 
 // Whenever there is a button in UI "toggle controller" is written b/c one time it will create object, other time it will delete object from database e.g here toggleSubscription will one time subscribe channel and other time unsubcribe channel.
 const toggleSubscription = asyncHandler(async (req, res) => {
@@ -31,19 +31,19 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, `Successfully ${result} channel!`));
 });
 
-// controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+// controller to return subscriber list of a channel (User= channel)
+const getChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
   const aggregationPipeline = await Subscription.aggregate([
     {
       $match: {
-        //using channel-field we are getting subscribers
+        //by using channel-field we are getting subscribers
         channel: new mongoose.Types.ObjectId(channelId),
       },
     },
     {
       $lookup: {
-        // here we are getting subscribers detail based on subscriber id
+        // here we are getting subscribers detail based on subscriberId
         from: "users",
         localField: "subscriber",
         foreignField: "_id",
@@ -77,11 +77,45 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 });
 
 // controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
-  const { subscriberId } = req.params;
+const getUserSubscribedChannels = asyncHandler(async (req, res) => {
+  const aggregationPipeline = await Subscription.aggregate([
+    { // by using subscriber field we are getting user subscribed channels
+      $match:{
+        subscriber : req.user._id
+      }
+    },
+    { // here we are getting channels details based on channelId
+      $lookup:{
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "channel"
+      }
+    },
+    {
+      $addFields:{
+        channel: { $arrayElemAt: ["$channel", 0] }
+      }
+    },
+    {
+      $project:{
+        channel:1
+      }
+    }
+    
+  ])
+  if (aggregationPipeline.length === 0) {
+    throw new ApiError("User has not subscribed to any channel", 400);
+  }
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        aggregationPipeline,
+        "Successfully fetched user subscribed channels!"
+      )
+    );
 });
 
-// *** After completed making above  3 controller i.e toggleSubscription, getUserChannelSubscribers, getSubscribedChannels
-//  WORK ON MAIKING getUserPlaylist() controller like getUserWatchHistroy()
-
-export { toggleSubscription, getUserChannelSubscribers };
+export { toggleSubscription, getChannelSubscribers, getUserSubscribedChannels };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 import {
   toggleSubscription,
-  getUserChannelSubscribers,
+  getChannelSubscribers,
+  getUserSubscribedChannels
 } from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/toggle-subscription/:channelId").post(toggleSubscription);
-router.route("/get-user-channel-subscribers/:channelId").get(getUserChannelSubscribers);
+router.route("/get-channel-subscribers/:channelId").get(getChannelSubscribers);
+router.route("/get-user-subscribed-channel").get(getUserSubscribedChannels);
 
 export default router;
